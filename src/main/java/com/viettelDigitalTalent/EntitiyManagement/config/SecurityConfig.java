e6daeaaebf409309de87e374048a24160db2dc9d
NghiaTran <nghiatran912005@gmail.com>
2026-06-27 15:05:12 +0700

    docker network

--- src/main/java/com/viettelDigitalTalent/EntitiyManagement/config/SecurityConfig.java
+++ src/main/java/com/viettelDigitalTalent/EntitiyManagement/config/SecurityConfig.java
@@ -28,7 +28,7 @@ public class SecurityConfig {
         return http
                 .cors(cors -> cors.configurationSource(request -> {
                     var config = new org.springframework.web.cors.CorsConfiguration();
-                    config.setAllowedOrigins(java.util.List.of("http://localhost:5173"));
+                    config.setAllowedOrigins(java.util.List.of("*"));
                     config.setAllowedMethods(java.util.List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
                     config.setAllowedHeaders(java.util.List.of("*"));
                     config.setAllowCredentials(true);
